47639f36f78d3859117e30a6379ceb484c920838
lanbow93 <the.doctor1501@gmail.com>
2023-02-24 10:52:06 -0600

    Controlled form now partially contains tags

--- src/pages/New.js
+++ src/pages/New.js
@@ -6,34 +6,47 @@ function New(props) {
     const addTag = () => {
         let data = [...tagFields, ""];
         setTagFields(data);
-     }
+    }
+    const [formInfo, setFormInfo] = useState ({
+        title: "",
+        owner: "",
+        deadline: "",
+        description: "",
+        tags: []
+    })
 
-    console.log(tagFields)
+    function updateTags(event, index) {
+        let tagArray = [...tagFields]
+        tagArray[index] = event.target.value
+        setTagFields(tagArray);
+    }
+
+
 
     return <Form action='/create' method='post' className='postFormField'>
     <h2>New Project Form</h2>
     <label className="bold">Title/Name: </label>
     <br />
-    <input type='input' name='title' placeholder='Project Name/Title' />
+    <input type='input' name='title' placeholder='Project Name/Title' value={formInfo.title} onChange={event => setFormInfo({...formInfo, title: event.target.value})} />
     <br />
     <label><span className="bold">Product Owner</span>: </label>
     <br />
-    <input type='input' name='owner' placeholder="Owner's Name"  />
+    <input type='input' name='owner' placeholder="Owner's Name" value={formInfo.owner} onChange={event => setFormInfo({...formInfo, owner: event.target.value})} />
     <br />
     <label>Deadline </label>
     <br />
-    <input type='date' name='deadline' placeholder='MM-DD-YYYY' />
+    <input type='date' name='deadline' placeholder='MM-DD-YYYY' value={formInfo.deadline} onChange={event => setFormInfo({...formInfo, deadline: event.target.value})}/>
     <br />
     <label>Description</label>
     <br />
-    <textarea  name='description' rows="2" />
+    <textarea  name='description' rows="2" value={formInfo.description} onChange={event => setFormInfo({...formInfo, description: event.target.value})} />
     <br />
     <label>Tag(s)</label>
     <br />
-    {tagFields.map(tag => { 
+    {tagFields.map((tag, index) => { 
         return <>
         <br />
-        <input className="tag" type='text' name='deadline' placeholder='New Tag' defaultValue={tag} />
+        <input className="tag" type='text' name='deadline' placeholder='New Tag' value={tag} onChange={event => updateTags(event, index)}/>
     
         </>
         })}
@@ -41,7 +54,7 @@ function New(props) {
         <p onClick={addTag}>+ New Tag</p>
         <br />
 
-    <input type="submit" value={props.value} />
+    <input type="submit"  />
 </Form>
 }
 
