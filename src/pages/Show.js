1cfb93660304ca21906a249568d75ff33be83905
lanbow93 <the.doctor1501@gmail.com>
2023-02-25 04:20:00 -0600

    calculated in for negative time

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,8 +5,8 @@ function Show(props){
     const date1 = new Date(project.deadline);
     const date2 = new Date();
     let timeDifference = Math.abs(date1 - date2) / 36e5;
-
-    const weeks = Math.floor(timeDifference / 168);
+    console.log(timeDifference)
+    const weeks = (date1 > date2) ? Math.floor(timeDifference / 168) : Math.round(timeDifference / -168);
     const days = Math.floor((timeDifference % 168) / 24 )
     const hours = Math.round((timeDifference % 168) % 24 )
 
